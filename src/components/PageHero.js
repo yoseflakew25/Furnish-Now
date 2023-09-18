57fe0a4b7f3f674c98b0e225526e3892a97bcaf6
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-09-18 08:57:48 +0300

    .

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -15,7 +15,7 @@ const PageHero = ({ title, product }) => {
 }
 
 const Wrapper = styled.section`
-  background: var(--clr-primary-10);
+  background: var(--clr-primary-8);
   width: 100%;
   min-height: 20vh;
   display: flex;
