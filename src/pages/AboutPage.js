047f05d74ae991e5b99d7d80b566eb48e552d1ef
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-09-18 15:47:21 +0300

    .

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,7 +10,7 @@ const AboutPage = () => {
         <img src={aboutImg} alt='nice desk' />
         <article>
           <div className='title'>
-            <h2>our story</h2>
+            <h2>About The Website</h2>
             <div className='underline'></div>
           </div>
           <p>
